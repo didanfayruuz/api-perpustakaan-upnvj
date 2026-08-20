cb0ef636ec182e76647795fd792027c59ec2c692
didanfayruuz <fayruuz06@gmail.com>
2026-08-20 19:06:30 +0700

    fix(deployment): fix vercel serverless function routing and export app in server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const app = require('./src/app');
-require('dotenv').config();
+const app = require("./src/app");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
     console.log(`Server API Perpustakaan berjalan di http://localhost:${PORT}`);
-});
+  });
+}
